4dd44b1767234b28648b1c0f9bbe846aa363a3df
Analyst <44039742+Atifqur@users.noreply.github.com>
2020-03-04 22:11:12 +0500

    Add files via upload

---
+++ run_analysis.R
@@ -0,0 +1,144 @@
+# Getting and Cleaning Data Course Project
+# Date : Wed Mar 04 21:50:51 2020
+# 
+# The purpose of this project is to demonstrate your ability to collect, work with, and clean a data set.
+# The goal is to prepare tidy data that can be used for later analysis. You will be graded by your peers
+# on a series of yes/no questions related to the project. You will be required to submit:
+# 1) a tidy data set as described below, 
+# 2) a link to a Github repository with your script for performing the analysis, and 
+# 3) a code book that describes the variables, the data, and any transformations or work that you performed
+#    to clean up the data called CodeBook.md. You should also include a README.md in the repo with your scripts.
+#    This repo explains how all of the scripts work and how they are connected.
+# 
+# One of the most exciting areas in all of data science right now is wearable computing - see for example
+# this article . Companies like Fitbit, Nike, and Jawbone Up are racing to develop the most advanced algorithms
+# to attract new users. The data linked to from the course website represent data collected from the
+# accelerometers from the Samsung Galaxy S smartphone. A full description is available at the site where
+# the data was obtained:
+# 
+# http://archive.ics.uci.edu/ml/datasets/Human+Activity+Recognition+Using+Smartphones
+# 
+# Here are the data for the project:
+# 
+# https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip
+# 
+# You should create one R script called run_analysis.R that does the following.
+# 
+# Merges the training and the test sets to create one data set.
+# Extracts only the measurements on the mean and standard deviation for each measurement.
+# Uses descriptive activity names to name the activities in the data set
+# Appropriately labels the data set with descriptive variable names.
+# From the data set in step 4, creates a second, independent tidy data set with the average of each variable
+# for each activity and each subject.
+# 
+# Good luck!
+
+# Loading libraries
+library(data.table)
+library(dplyr)
+
+# Setting working Directory
+setwd("C:\\Users\\Documents\\Data")
+
+# Creating folder and Downloading UCI data files from the web
+folder<-"./cleaning_n_getting_Dataset_project"
+if(!dir.exists(folder)) {
+dir.create(folder)}
+
+# Setting working Directory
+setwd("C:\\Users\\Documents\\Data\\cleaning_n_getting_Dataset_project")
+
+URL <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+destFile <- "./project_Dataset.zip"
+if(!file.exists(destFile))
+{
+download.file(URL, destfile = destFile, mode="wb")
+}
+
+# Unzipping data files
+if (!file.exists("UCI HAR Dataset")){
+  unzip(destFile)
+}
+
+# Specifying time/date
+dateDownloaded <- date()
+
+
+# Reading Activity labels code for test and train data. code ranges from 1 to 6
+# 1 WALKING
+# 2 WALKING_UPSTAIRS
+# 3 WALKING_DOWNSTAIRS
+# 4 SITTING
+# 5 STANDING
+# 6 LAYING
+y_test <- read.table("./UCI HAR Dataset/test/y_test.txt", header = FALSE)
+y_train <- read.table("./UCI HAR Dataset/train/y_train.txt", header = FALSE)
+
+
+# Reading 561 features data in coloumns which is normalized and bounded within [-1,1]
+feature_test <- read.table("./UCI HAR Dataset/test/X_test.txt", header = FALSE)
+feature_train <- read.table("./UCI HAR Dataset/train/X_train.txt", header = FALSE)
+
+
+# Reading Each row of subject who performed the activity for each window sample, ranges from 1 to 30
+# Train contains 70% and Test contains 30% subjects
+subject_test <- read.table("./UCI HAR Dataset/test/subject_test.txt", header = FALSE)
+subject_train <- read.table("./UCI HAR Dataset/train/subject_train.txt", header = FALSE)
+
+
+# Reading 6 Activity Label Names
+activity_labels <- read.table("./UCI HAR Dataset/activity_labels.txt", header = FALSE)
+
+
+# Reading 561 Features Name
+features_name <- read.table("./UCI HAR Dataset/features.txt", header = FALSE)
+
+
+# Merging the training (70%) and the test (30%) data sets to create one data set (100%)
+feature_dat <- rbind(feature_test, feature_train)   # Data comprised of 561 features name
+subject_dat <- rbind(subject_test, subject_train)   # Subject ranges from 1 to 30
+activity_dat <- rbind(y_test, y_train)# activity ranges from 1 to 6
+
+# Renaming columns in activity_dat & activity_labels dataframes
+names(activity_dat) <- "activity_code"
+names(activity_labels) <- c("activity_code", "Activity")
+
+# Get factor of Activity names
+Activity <- left_join(activity_dat, activity_labels, "activity_code")[, 2]
+
+# Renaming subject_dat columns
+names(subject_dat) <- "Subject"
+
+# Renaming feature_dat columns using columns from features_name
+names(feature_dat) <- features_name$V2
+
+
+# Create one large Dataset with only these variables: subject_dat,  Activity,  feature_dat
+Large_dataset <- cbind(subject_dat, Activity,feature_dat)
+View(Large_dataset)
+# Creating New datasets by extracting only the measurements on the mean and
+# standard deviation for each measurement.
+features_name_mean_std <- features_name$V2[grep("\\bmean()\\b|\\bstd()\\b", features_name$V2)]
+View(features_name_mean_std)
+
+DataNames <- c("Subject", "Activity", as.character(features_name_mean_std))
+View(DataNames)
+
+Large_dataset <- subset(Large_dataset, select=DataNames)
+View(Large_dataset)
+
+# Rename the columns of the large_dataset using more descriptive activity names
+names(Large_dataset)<-gsub("^t", "Time", names(Large_dataset))
+names(Large_dataset)<-gsub("^f", "Frequency", names(Large_dataset))
+names(Large_dataset)<-gsub("Acc", "Accelerometer", names(Large_dataset))
+names(Large_dataset)<-gsub("Gyro", "Gyroscope", names(Large_dataset))
+names(Large_dataset)<-gsub("Mag", "Magnitude", names(Large_dataset))
+names(Large_dataset)<-gsub("BodyBody", "Body", names(Large_dataset))
+
+# Create a second, independent tidy data set with the average of each variable for each activity and each subject
+SecondLarge_dataset<-aggregate(. ~Subject + Activity, Large_dataset, mean)
+SecondLarge_dataset<-SecondLarge_dataset[order(SecondLarge_dataset$Subject,SecondLarge_dataset$Activity),]
+View(SecondLarge_dataset)
+
+# Save this tidy dataset to local file
+write.table(SecondLarge_dataset, file = "tidydata.txt",row.name=FALSE)
